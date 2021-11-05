cdf1c57d348f838405b2d85e7eed47ae4dc30db6
huanghongru <huanghongru1997@gmail.com>
2021-11-05 15:23:11 +0800

    first-commit

--- main.py
+++ main.py
@@ -1,28 +1,80 @@
+# reference ==> https://www.shadertoy.com/view/MdXSzS
+
 import taichi as ti
+import handy_shader_functions as hsf
 
-ti.init(arch=ti.gpu)
+ti.init(arch = ti.cuda, debug=True)
 
-n = 320
-pixels = ti.field(dtype=float, shape=(n * 2, n))
+res_x = 1200
+res_y = 675
+pixels = ti.Vector.field(3, ti.f32, shape=(res_x, res_y))
 
 @ti.func
-def complex_sqr(z):
-    return ti.Vector([z[0]**2 - z[1]**2, z[1] * z[0] * 2])
+def bh_smoothstep(edge1, edge2, v):
+    assert(edge1 != edge2)
+    t = (v-edge1) / float(edge2-edge1)
+    t = hsf.clamp(t, 0.0, 1.0)
+
+    return (5-4 * t) * t**2.4
 
 @ti.kernel
-def paint(t: float):
-    for i, j in pixels:  # Parallelized over all pixels
-        c = ti.Vector([-0.8, ti.cos(t) * 0.2])
-        z = ti.Vector([i / n - 1, j / n - 0.5]) * 2
-        iterations = 0
-        while z.norm() < 20 and iterations < 50:
-            z = complex_sqr(z) + c
-            iterations += 1
-        pixels[i, j] = 1 - iterations * 0.02
+def render(time:ti.f32):
+    # draw something on your canvas
+    for i,j in pixels:
+        color = ti.Vector([0.0, 0.0, 0.0]) # init your canvas to black
 
-gui = ti.GUI("Julia Set", res=(n * 2, n))
+        uv = ti.Vector([float(i) / res_x, float(j) / res_y]) - 0.5 # putting everything between -0.5 and 0.5
+        t = time * 0.1 + ((0.25 + 0.05 * ti.sin(time * 0.1))/(uv.norm() + 0.07)) * 2.2;
+        si = ti.sin(t)
+        co = ti.cos(t)
+        ma = ti.Matrix([[co, si], [-si, co]])
 
-for i in range(1000000):
-    paint(i * 0.03)
+        v1 = v2 = v3 = 0.0
+        s = 0.0
+
+        for k in range(90):
+            p = s * uv
+            ps = s
+
+            p = ma @ p # rotate
+            p += ti.Vector([0.22, 0.3])
+            ps += s - 1.5 - ti.sin(time * 0.13) * 0.1
+
+            # draw spiral curves
+            for l in range(8):
+                len2 = p.dot(p) + ps * ps
+                p = ti.abs(p) / len2 - 0.659
+                ps = ti.abs(ps) / len2 - 0.659
+            
+            len2 = p.dot(p) + ps * ps
+            v1 += len2 * 0.0015 * (1.8 + ti.sin(uv.norm() * 13.0) + 0.5 - time * 0.2)
+            v2 += len2 * 0.0013 * (1.5 + ti.sin(uv.norm() * 14.5) + 1.2 - time * 0.3)
+            v3 += p.norm() * 0.003
+            s += 0.035 
+
+        len = uv.norm()
+        # v1 *= hsf.smoothstep(0.8, 0.0, len)
+        # v2 *= hsf.smoothstep(0.6, 0.0, len)
+        # v3 *= hsf.smoothstep(0.9, 0.0, len)
+	
+        color[0] = v3 * (1.5 + ti.sin(time * 0.2) * 0.4)
+        color[1] = (v1 + v3) * 0.3
+        color[2] = v2
+        color += hsf.smoothstep(0.2, 0.0, len) * 0.85 + hsf.smoothstep(0.0, 0.6, v3) * .3
+
+        r = (uv * ti.Vector([0.5, 0.7])).norm()
+        r_ = (uv * ti.Vector([0.8, 0.5])).norm()
+        if r <2.4e-2:
+            color = ti.Vector([0.0, 0.0, 0.0]) + bh_smoothstep(0, 2.4e-2, r_)
+            #color = ti.Vector([1.0, 1.0, 1.0]) * bh_smoothstep(0, 2.4e-2, r_)
+        else:
+            color = hsf.clamp(color, 0.0, 1.0)
+        pixels[i, j] = color
+
+gui = ti.GUI("Canvas", res=(res_x, res_y))
+
+for i in range(100000):
+    t = i * 0.03
+    render(t)
     gui.set_image(pixels)
     gui.show()
